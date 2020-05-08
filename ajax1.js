a5a8746a52f12667999a11c61636e10c1b9fee30
Alexandr <shalser82@gmail.com>
2020-05-08 08:15:30 +0300

    fix ajax1.js

--- ajax1.js
+++ ajax1.js
@@ -1,39 +1,37 @@
-const requestURL = 'sort.php'
-
-function sendRequest(method, url, body = null) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest()
-
-        xhr.open(method, url)
-
-        xhr.responseType = 'json'
-        xhr.setRequestHeader('Content-Type', 'application/json')
-
-        xhr.onload = () => {
-            if (xhr.status >= 400) {
-                reject(xhr.response)
-            } else {
-                resolve(xhr.response)
+// $(document).ready(function () {
+//     $('#submit').on('click', function () {
+//         let name = $('input#name:checked').val();
+//
+//         console.log(name);
+//     })
+// })
+// ---------------------
+// $(document).ready(function () {
+//
+//     if ($('input#name').is(":checked")) {
+//             let name = $('input#name:checked').val();
+//             console.log(name);
+//     }
+// })
+// --------------------------
+$(document).ready(function(){
+    $('#submit').click(function(){
+        var name = [];
+        $('#name').each(function(){
+            if($(this).is(":checked"))
+            {
+                name.push($(this).val());
             }
-        }
+        });
+        name = name.toString();
+        $.ajax({
+            url:"sort.php",
+            method:"POST",
+            data:{name:name},
+            success:function(data){
+                $('#result').html(data);
+            }
+        });
+    });
+});
 
-        xhr.onerror = () => {
-            reject(xhr.response)
-        }
-
-        xhr.send(JSON.stringify(body))
-    })
-}
-
-sendRequest('GET', requestURL)
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-
-const body = {
-    name: 'Vladilen',
-    age: 26
-}
-
-sendRequest('POST', requestURL, body)
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
